43b3ee8392475d1b1800e52756f7aad0fb30a2a9
Travis Rollins <TROLL59910@aol.com>
2017-10-25 17:39:34 -0600

    Setup redux and router

--- src/Pages/Main/MainSection/MainSection.js
+++ src/Pages/Main/MainSection/MainSection.js
@@ -4,7 +4,7 @@ import './MainSection.css';
 const MainSection = ({ articleClass, src, title, info }) => {
   return (
     <article className={`main-section ${articleClass}`}>
-      <img src={src} class="main-section-image" />
+      <img src={src} className="main-section-image" alt=""/>
       <section className="content">
         <h2>
           {title}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { ConnectedRouter } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const history = createHistory()
+
+const store = createStore(
+  rootReducer,
+  devTools,
+  applyMiddleware(thunk),
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
