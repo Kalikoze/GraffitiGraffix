908c29faf3a1a28faa7e98609928b53cc4232fa5
Tyler Hevia <tylerhevia@gmail.com>
2017-10-30 14:00:11 -0600

    :fire: Fix seed data :fire:
    - Seed all data from one seed file

---
+++ db/seeds/MockSeedData/MockCommentData.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    comment: 'Hello bro!',
+    user_id: 1,
+    image_id: 1
+  },
+  {
+    id: 2,
+    comment: "It's Lit!",
+    user_id: 2,
+    image_id: 1
+  },
+  {
+    id: 3,
+    comment: 'Dude bruh',
+    user_id: 1,
+    image_id: 1
+  }
+];
---
+++ db/seeds/MockSeedData/MockFollowerData.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    follower_id: 1,
+    artist_id: 2
+  },
+  {
+    id: 2,
+    follower_id: 1,
+    artist_id: 3
+  },
+  {
+    id: 3,
+    follower_id: 2,
+    artist_id: 1
+  }
+];
