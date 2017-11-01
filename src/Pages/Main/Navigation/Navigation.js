5dd38c67ad4c240f44e279ba34194c20e4bc501a
Tyler Hevia <tylerhevia@gmail.com>
2017-10-31 18:39:44 -0600

    :fire: Store current user in redux store :fire:
    	- Log in with google, check if user's google_uid exists in our database
    	- If so, retrieve user from database and update currentUser in store
    	- If google_uid does not exist, set currentUser to null and go to sign up page
    	- Still need to create sign in component

--- src/Pages/Main/Navigation/Navigation.js
+++ src/Pages/Main/Navigation/Navigation.js
@@ -4,45 +4,42 @@ import firebase, { signIn, signOut } from '../../../firebase.js';
 import './Navigation.css';
 
 export default class Navigation extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       search: ''
     };
   }
 
   fetchUID(googleUID) {
-    fetch(`http://localhost:3001/api/v1/users/auth/${googleUID}`)
+    fetch(`http://localhost:3001/api/v1/users/auth/666`)
       .then(response => response.json())
-      .then(parsedResponse => handleUIDCheck(parsedResponse))
+      .then(parsedResponse => this.handleUIDCheck(parsedResponse))
+      .catch(error => console.log(error));
   }
 
   handleUIDCheck(response) {
+    console.log('user', response);
     if (response.error) {
       //send user to another page that would prompt them to make choose a username, add a shortBio, add a tag (Add name from google)
       //create account (response.google_uid)
+      return this.props.storeCurrentUser(null);
       console.log(response.error);
+    } else {
+      this.props.storeCurrentUser(response);
     }
-    //grab user data and put in store as loggedInUser
-    //create action to do this
   }
 
   sendSignInData() {
-    signIn()
-      .then(response => {
-        console.log('response', response.user.uid);
-        this.fetchUID(response.user.uid)
-      })
+    signIn().then(response => this.fetchUID(response.user.uid));
   }
 
   signOutUser() {
-    signOut()
-      .then(() => {
-        console.log('logout response');
-        //set loggedInUser to ''
-      })
+    signOut().then(() => {
+      console.log('logout response');
+      this.props.storeCurrentUser(null);
+    });
   }
-
 
   render() {
     const { search } = this.state;
@@ -75,12 +72,20 @@ export default class Navigation extends Component {
         </div>
 
         <div className="nav-section">
-          <NavLink to="/" className="sign-up-link link" onClick={() => this.sendSignInData()}>
+          <NavLink
+            to="/"
+            className="sign-up-link link"
+            onClick={() => this.sendSignInData()}
+          >
             Sign Up
           </NavLink>
         </div>
         <div className="nav-section">
-          <NavLink to="/" className="sign-up-link link" onClick={() => this.signOutUser()}>
+          <NavLink
+            to="/"
+            className="sign-up-link link"
+            onClick={() => this.signOutUser()}
+          >
             Sign Out
           </NavLink>
         </div>
