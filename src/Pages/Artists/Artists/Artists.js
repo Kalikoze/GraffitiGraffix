c47cc9e10802dddfa7ba964a45fce940bbd61b3a
Travis Rollins <TROLL59910@aol.com>
2017-11-01 16:42:19 -0600

    Fetch artists and their images on componendDidMount when on Artist page.

--- src/Pages/Artists/Artists/Artists.js
+++ src/Pages/Artists/Artists/Artists.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Filter from '../Filter/Filter'
 import './Artists.css'
-import helperArtists from '../../../helpers/helperArtists';
 import SingleArtist from '../SingleArtist/SingleArtist';
 
 export default class Artists extends Component {
@@ -13,13 +12,31 @@ export default class Artists extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      artists: helperArtists
+    return Promise.all([this.fetchArtists(), this.fetchImages()])
+    .then(response => this.setState({artists: this.assignImages(response[0], response[1])}))
+  }
+
+  fetchArtists() {
+    return fetch('http://localhost:3001/api/v1/users')
+      .then(response => response.json())
+      .then(artists => artists)
+  }
+
+  fetchImages() {
+    return fetch('http://localhost:3001/api/v1/images')
+      .then(response => response.json())
+      .then(images => images)
+  }
+
+  assignImages(artists, images) {
+    return artists.map((artist, i) => {
+      const artistImages = images.filter(image => image.user_id === artist.id);
+      return Object.assign({}, artist, {latestImages: artistImages.slice(0, 3)})
     })
   }
 
   render() {
-    const artistList = helperArtists.map((artist, i) => <SingleArtist key={i} {...artist}/>);
+    const artistList = this.state.artists.map((artist, i) => <SingleArtist key={i} {...artist}/>);
 
     return (
       <section className="l-artists">
