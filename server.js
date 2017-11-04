6357d56a530a46bd2dc15ead1c62a487d5c4f99a
Travis Rollins <TROLL59910@aol.com>
2017-11-04 17:00:23 -0600

    Test get method for users

--- server.js
+++ server.js
@@ -234,3 +234,5 @@ app.patch('/api/v1/comments/:id', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
