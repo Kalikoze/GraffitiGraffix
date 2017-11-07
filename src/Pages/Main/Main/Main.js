0b3ec7a6c837656a902e5e1c7e5aee99c0719224
David Becker <david@beckersite.com>
2017-11-07 15:24:29 -0700

    Add content to main page.

--- src/Pages/Main/Main/Main.js
+++ src/Pages/Main/Main/Main.js
@@ -19,21 +19,21 @@ const Main = () =>
     <section className="l-below-fold">
       <MainSection
         src={PaintSplashLeft}
-        title="Latest News"
-        articleClass="news"
-        info="hey"
+        title="The Latest & Greatest"
+        articleClass="latest"
+        info="See artist's latest and greatest works.  Filter through artists and find who's most popular and who's new!  Get to know your competition, and be relevant in the GraffitiGraffix community."
       />
       <MainSection
         src={PaintsplashRight}
-        title="What's Hot"
-        articleClass="whats-hot"
-        info="yo"
+        title="Be Seen, Get Known"
+        articleClass="seen"
+        info="Create your own profile, upload your own pictures, and get known by your own personal tag.  Get to the top of the charts by getting other users to follow you!"
       />
       <MainSection
         src={PaintExplosion}
-        title="Artists"
-        articleClass="artists"
-        info="suh"
+        title="Choose Your Experience"
+        articleClass="experience"
+        info="Follow the artists that you care about most, and keep up to date with their latest graffiti postings.  Create the community by being the community."
       />
     </section>
    </div>);
