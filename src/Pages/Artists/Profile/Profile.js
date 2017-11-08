5f99a7184415abad61259ed4c3650e9ee1155b99
David Becker <david@beckersite.com>
2017-11-07 20:10:18 -0700

    Fix follow function to handle both onload and click of button

--- src/Pages/Artists/Profile/Profile.js
+++ src/Pages/Artists/Profile/Profile.js
@@ -30,7 +30,14 @@ class Profile extends Component {
 
     fetch(`/api/v1/followers/${clickedArtist.id}`)
       .then(response => response.json())
-      .then(followers => this.setState({ followers }))
+      .then(followers => {
+
+        if (followers.length) {
+          this.setState({ followers }, () => {
+            this.checkIfFollowing(true)
+          })
+        }
+      })
       .catch(error => console.log({ error }));
   }
 
@@ -147,7 +154,7 @@ class Profile extends Component {
     });
   }
 
-  checkIfFollowing() {
+  checkIfFollowing(onLoad) {
     const { followers } = this.state;
     const { id: artist_id } = this.props.clickedArtist;
     const { id: follower_id } = this.props.currentUser;
@@ -157,6 +164,10 @@ class Profile extends Component {
         follower.artist_id === artist_id &&
         follower.follower_id === follower_id,
     );
+
+    if (onLoad) {
+      return this.setState({ followStatus: true });
+    }
 
     return isFollowed !== -1 ? this.unfollowArtist() : this.followArtist();
   }
